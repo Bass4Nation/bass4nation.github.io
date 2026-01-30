1c26b935193666ea8285b7097f5ad2dd1d1d97b0
kss-zolw <135342985+kss-zolw@users.noreply.github.com>
2026-01-30 21:45:58 +0100

    Starting on navbar

---
+++ app/nav.tsx
@@ -0,0 +1,19 @@
+import style from './Styles/Nav.module.css'
+
+let tuplet1: [number,boolean, string];
+
+
+const links = ['Frontpage', 'About me', 'Github repos']
+
+export default function Nav() {
+  return (
+    <nav className={style.navbox} >
+        <h1 className={style.toptitle}>Kristoffer Snopestad Søderkvist portfolio page</h1>
+        <h2 className={style.undertitle}>portfolio page</h2>
+        <ul className={style.navbar}>
+            {links.map((text) =>
+            <li className={style.navitem}>{text}</li>
+            )}
+        </ul>
+    </nav>
+  )};
