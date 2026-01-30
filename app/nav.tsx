d8418587cdfa9e7c8c6020ab63512e4f0307c4b5
kss-zolw <135342985+kss-zolw@users.noreply.github.com>
2026-01-30 21:53:18 +0100

    Under construction

--- app/nav.tsx
+++ app/nav.tsx
@@ -9,7 +9,7 @@ export default function Nav() {
   return (
     <nav className={style.navbox} >
         <h1 className={style.toptitle}>Kristoffer Snopestad Søderkvist</h1>
-        <h2 className={style.undertitle}>portfolio page</h2>
+        <h2 className={style.undertitle}>portfolio page - Under construction</h2>
         <ul className={style.navbar}>
             {links.map((text) =>
             <li className={style.navitem}>{text}</li>
